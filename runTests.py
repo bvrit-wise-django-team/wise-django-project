e241396de551b495af7c48a1ba15293f437db8bd
Nandiniy14 <14wh1a0597@bvrithyderabad.edu.in>
2017-01-18 23:09:48 +0530

    Update runTests.py

--- runTests.py
+++ runTests.py
@@ -1,8 +1,25 @@
-def multiply(a, b):
-	return a * b;
+#def multiply(a, b):
+#	return a * b;
 
-def test_numbers_3_4():
-    assert multiply(4, 5) == 20
-    assert multiply(4, 7) == 28
+#def test_numbers_3_4():
+#   assert multiply(4, 5) == 20
+#    assert multiply(4, 7) == 28
+	
+from django.test import TestCase
+from remarks.models import Remarks
+from django.utils import timezone
+#from django.core.urlresolvers import reverse
+
+
+# models test
+class RemarksTest(TestCase):
+
+    def create_remarks(self, title="only a test", body="yes, this is only a test"):
+        return Remarks.objects.create(title=title, body=body, created_at=timezone.now())
+
+    def test_remarks_creation(self):
+        w = self.create_remarks()
+        self.assertTrue(isinstance(w, Remarks))
+        self.assertEqual(w.__unicode__(), w.title)
 	
 
