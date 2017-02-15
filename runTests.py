d56031d629ed00880d3a3746caa7b5830518fb15
SushmaK <14wh1a0582@bvrithyderabad.edu.in>
2017-02-15 21:14:23 +0530

    updated

--- runTests.py
+++ runTests.py
@@ -1,6 +1,21 @@
+#sampleTests
 def multiply(a , b):
 	return a * b;
 
 def test_numbers_3_4():
     assert multiply(4, 5) == 20
     assert multiply(4, 7) == 28
+	
+from django.test import TestCase
+from myapp.models import Student
+
+class StudentTestCase(TestCase):
+    def setUp(self):
+        Student.objects.create(name="xyz") 
+	Student.objects.create(qualification="b.tech")
+def test_student_details(self):
+        name1 = Student.objects.get(name="xyz")
+        quali1 = Student.objects.get(name="b.tech")
+	self.assertEqual(name1.name(), 'xyz"')
+        self.assertEqual(quali.qualification(), 'b.tech"')
+
