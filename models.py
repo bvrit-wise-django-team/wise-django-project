723aaee994348f92924dd8e918bbd252be655ba8
AravindaMucha <14wh1a0204@bvrithyderabad.edu.in>
2017-02-16 08:27:43 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-
 class Remarks(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
@@ -10,10 +9,11 @@ class Remarks(models.Model):
     published_date = models.DateTimeField(
             blank=True, null=True)
 
+    
     def publish(self):
         self.published_date = timezone.now()
         self.save()
-
+      
     def __str__(self):
         return self.title
-        
+       
